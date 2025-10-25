8b95a06038e3b020fe012d36bc733de0ba01bb7e
Hammurabi <38507441+Hammurabi@users.noreply.github.com>
2025-10-25 13:10:13 +0300

    Delete Parser.java

--- src/main/java/io/adenium/papaya/parser/Parser.java
+++
@@ -1,9 +0,0 @@
-package io.adenium.papaya.parser;
-
-import io.adenium.papaya.compiler.AbstractSyntaxTree;
-import io.adenium.papaya.compiler.TokenStream;
-import io.adenium.exceptions.PapayaException;
-
-public interface Parser {
-    AbstractSyntaxTree parse(TokenStream stream) throws PapayaException;
-}
