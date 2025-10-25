85ba2eb87e5fd76fbe9c74f8e941bf58a772286d
Hammurabi <38507441+Hammurabi@users.noreply.github.com>
2025-10-25 13:20:06 +0300

    Delete Traverser.java

--- src/main/java/io/adenium/papaya/compiler/Traverser.java
+++
@@ -1,8 +0,0 @@
-package io.adenium.papaya.compiler;
-
-import io.adenium.exceptions.PapayaException;
-import io.adenium.papaya.parser.Node;
-
-public interface Traverser {
-    void onEnter(Node node, FunctionScope functionScope, CompilationScope compilationScope) throws PapayaException;
-}
