75cd8b8dbd130aaaef3fbe2c816eda4b8ea6efeb
Hammurabi <38507441+Hammurabi@users.noreply.github.com>
2025-10-25 13:09:43 +0300

    Delete PapayaCallable.java

--- src/main/java/io/adenium/papaya/runtime/PapayaCallable.java
+++
@@ -1,8 +0,0 @@
-package io.adenium.papaya.runtime;
-
-import io.adenium.exceptions.PapayaException;
-
-public interface PapayaCallable {
-    public static final PapayaCallable Default = scope -> {};
-    public void call(Scope scope) throws PapayaException;
-}
